fbf25b2500e10648fe59389f46e0ef561ede8d10
Aleksei Chusovliankin <lixich@mail.ru>
2018-03-15 11:59:40 +0500

    Update geocodeYandex.py

--- geocodeYandex.py
+++ geocodeYandex.py
@@ -1,6 +1,5 @@
 #https://tech.yandex.ru/maps/geocoder/
 import requests
 url = 'https://geocode-maps.yandex.ru/1.x/'
-r = requests.get(url, params = {'geocode': 'Пермь, Техническая, 22', 'format':'json'})
-print(r.json())
+r = requests.get(url, params = {'geocode': 'Perm', 'format':'json'})
 print(r.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['Point']['pos'])
